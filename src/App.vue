750628d21cf400e92b5b3ddc1e984a983e3c72d2
Bastian Schwarzmüller <bastian.schw@rzmueller.de>
2020-11-07 21:34:53 +0100

    Chapter1 - Chapter3
    Setup Project and create first components

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }} <strong>Followers: </strong>
+    {{ followers }}
+    <button @click="followUser">Follow</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "_NopeMitchell",
+        firstName: "Hans-Peter",
+        lastName: "Meier",
+        email: "deinemudder@gmx.de",
+        isAdmin: true,
+      },
+    };
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has a new Follower!`);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
